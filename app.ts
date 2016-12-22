20841f3bbf13d183098d0fe3cc97eda8bbd83930
gotrecillo <sergio.panadero.perez@gmail.com>
2016-12-22 22:27:33 +0100

    hello world working

--- app.ts
+++ app.ts
@@ -1,3 +1,26 @@
-/**
- * Created by gotre on 22/12/16.
- */
+import { NgModule, Component } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+
+@Component({
+    selector : 'hello-world',
+    template : `
+        <div>
+            Hello world
+        </div>
+        `,
+})
+class HelloWorld {
+
+}
+
+@NgModule({
+    declarations : [ HelloWorld ],
+    imports      : [ BrowserModule ],
+    bootstrap    : [ HelloWorld ],
+})
+
+class HelloWorldAppModule {
+}
+
+platformBrowserDynamic().bootstrapModule(HelloWorldAppModule);
